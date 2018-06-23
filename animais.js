1a9535fea58c8b7548a1ea3e9674bcabad3165dd
JP Bochi <jpbochi@protonmail.ch>
2018-06-23 13:42:27 +0100

    adds pieces

---
+++ animais.js
@@ -0,0 +1,84 @@
+const list = () => [
+  [
+    '..GG',
+    '...G',
+    '...G',
+    '...G',
+    '...G',
+    'GGGG',
+    '.GGG',
+    '.G.G',
+    '.G.G'
+  ], // Girafa
+  [
+    'EEEEEE',
+    'EEEEEE',
+    'EEEE.E',
+    'E..E.E',
+    'E..E.E'
+  ], // Elefante
+  [
+    '.....DD',
+    '......D',
+    '......D',
+    '......D',
+    '...DDDD',
+    'DDDDDDD',
+    '...D..D'
+  ], // Dinosauro
+  [
+    'HHHHHH',
+    'HHHHHH',
+    '.H.H..'
+  ], // Hipopotamo
+  [
+    '.M.MM',
+    'MMMM',
+    'M.M',
+    'M.M',
+    'M.M'
+  ], // caMelo
+  [
+    '...ZZ',
+    'ZZZZ',
+    'Z..Z',
+    'Z..Z'
+  ], // Zebra
+  [
+    'UUUUU',
+    'UUUU',
+    'U..U'
+  ], // Urso marrom
+  [
+    'BBBB',
+    'BBB',
+    'B.B'
+  ], // urso Branco
+  [
+    'CCCCC',
+    'C.C'
+  ], // Crocodilo
+  [
+    '..PP',
+    'PPP',
+    'PPP'
+  ], // Pato
+  [
+    '.X.',
+    'XXX',
+    '.X.',
+    '.X.'
+  ], // cruX
+  [
+    'OOO',
+    'OO'
+  ], // caracOl
+  [
+    'FF',
+    'F'
+  ] // Fish
+];
+
+module.exports = {
+  list
+};
